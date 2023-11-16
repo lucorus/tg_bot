2a7b1291a506d75f0fd405df7439681e861de89b
lucorus <nikita.hom1977@gmail.com>
2023-11-16 19:36:14 +0400

    add inline buttons and docker

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot
 import sqlite3
-from telebot import types
 import config
 
 bot = telebot.TeleBot(config.token)
@@ -14,87 +13,46 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS files
                    file BLOB)''')
 
 
+admins = [1677887525, ]
+
+
 # выводит список файлов в бд
 @bot.message_handler(commands=['list'])
 def list_files_command(message):
     cursor.execute("SELECT name FROM files")
     files = cursor.fetchall()
     if files:
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for file in files:
-            markup.add(types.KeyboardButton(file[0]))
-        file_names = [file[0] for file in files]
-        bot.reply_to(message, f"Список файлов:\n\n{', '.join(file_names)}", reply_markup=markup)
-        bot.set_state(message.chat.id, 'list')
-        bot.register_next_step_handler(message, get_file)
+        markup = telebot.types.InlineKeyboardMarkup()
+        for file_name in files:
+            button = telebot.types.InlineKeyboardButton(text=file_name[0], callback_data=file_name[0])
+            markup.add(button)
+        bot.send_message(message.chat.id, 'Выберите файл:', reply_markup=markup)
     else:
         bot.reply_to(message, "В базе данных нет файлов.")
 
 
-# выводит кнопочки
-def get_file(message):
+# отправляет файл
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback_query(call):
     try:
-        file_name = message.text
-        cursor.execute("SELECT * FROM files WHERE name= ?", (file_name, ))
-        file_content = cursor.fetchone()[2]
-        bot.send_document(message.chat.id, file_content)
-    except:
-        pass
+        cursor.execute("SELECT name, file FROM files WHERE name= ?", (call.data, ))
+        file = cursor.fetchall()
+        # если файл имеет тип png/jpg, то отправляем его картинкой
+        if str(file[0][0])[-4:] == ".png" or str(file[0][0])[-4:] == ".jpg":
+            # отправляем фото
+            bot.send_photo(call.message.chat.id, photo=file[0])
+        else:
+            # отправляем документ
+            bot.send_document(call.message.chat.id, file[0])
+    except Exception as e:
+        bot.send_message(call.message.chat.id, f'Ошибка при отправке файла: {e}')
 
 
-# берёт файл с переданным названием
-@bot.message_handler(commands=['get'])
-def get_file_command(message):
-    file_name = message.text.split(' ')[1]
-    cursor.execute("SELECT file FROM files WHERE name=?", (file_name,))
-    file_data = cursor.fetchone()
-    if file_data:
-        file_bytes = file_data[0]
-        bot.send_document(message.chat.id, file_bytes)
-    else:
-        bot.reply_to(message, "Файл с указанным названием не найден.")
-
-
-# добавляет документ с переданным названием
-@bot.message_handler(content_types=['document', 'string'])
-def add_document(message):
-    if message.from_user.id == 1677887525:
-        try:
-            if message.document:
-                # Получаем информацию о документе
-                file_id = message.document.file_id
-                file_info = bot.get_file(file_id)
-                file_path = file_info.file_path
-
-                # Скачиваем документ
-                downloaded_file = bot.download_file(file_path)
-
-                # проверяем есть ли файл с таким названием в бд
-                cursor.execute('SELECT * FROM files WHERE name=?', (message.caption, ))
-                ft = cursor.fetchall()
-                if ft:
-                    # файл существует
-                    bot.reply_to(message, 'Файл с таким названием уже существует')
-                else:
-                    # Сохраняем документ в базе данных
-                    cursor.execute("INSERT INTO files (name, file) VALUES (?, ?)", (message.caption, downloaded_file))
-                    conn.commit()
-
-                    bot.reply_to(message, 'Файл сохранен в базе данных!')
-            else:
-                bot.reply_to(message, 'Вы не прикрепили файл')
-        except:
-            bot.send_message(message.chat.id, 'Произошла неизвестная ошибка!')
-    else:
-        bot.reply_to(message, 'У вас нет прав для данного действия!')
-
-
-# удаляет из бд документ с переданным названием
+# удаляет документ из бд
 @bot.message_handler(commands=['delete'])
 def delete_document(message):
     name = str(message.text)[8:]
-    print(name)
-    if message.from_user.id == 1677887525:
+    if message.from_user.id in admins:
         try:
             cursor.execute('DELETE FROM files WHERE name=?', (name, ))
             conn.commit()
@@ -109,7 +67,7 @@ def delete_document(message):
 @bot.message_handler(commands=['clear'])
 def clear_documents(message):
     print('База данных очищена')
-    if message.from_user.id == 1677887525:
+    if message.from_user.id in admins:
         try:
             cursor.execute('DELETE FROM files')
             conn.commit()
@@ -120,9 +78,56 @@ def clear_documents(message):
         bot.reply_to(message, f'У вас нет прав для данного действия')
 
 
+# получаем pk user'a
 @bot.message_handler(commands=['pk'])
 def user_pk(message):
     bot.send_message(message.chat.id, f'Ваш pk = { message.from_user.id }')
+
+
+# сохраняем файл
+# если файл отправляется без сжатия, то нужно указать имя файла без расширения
+# если файл отправляется с сжатием, то нужно указать имя файла + желаемое расширение
+# (если этого не сделать, то будет выбрано имя, отправленного файла)
+@bot.message_handler(content_types=['document', 'photo'])
+def handle_files(message):
+    if message.from_user.id in admins:
+        try:
+            if message.caption:
+                file_name = message.caption
+            else:
+                if message.document:
+                    file_name = message.document.file_name
+                else:
+                    # если пользователь не указал имя для изображения,
+                    # то выбрасываем исключение
+                    raise Exception('Вы не указалии имя для файла')
+
+            # если нужно сохранить в бд документ
+            if message.document:
+                # Получаем информацию о документе
+                file_info = bot.get_file(message.document.file_id)
+                downloaded_file = bot.download_file(file_info.file_path)
+
+                # Сохраняем документ в бд
+                cursor.execute("INSERT INTO files (name, file) VALUES (?, ?)", (file_name, downloaded_file))
+                conn.commit()
+
+                bot.reply_to(message, f'Документ "{file_name}" успешно сохранен.')
+            # если нужно сохранить фото в бд
+            elif message.photo:
+                # Получаем информацию о фото
+                file_info = bot.get_file(message.photo[-1].file_id)
+
+                # Сохраняем фото в бд
+                downloaded_file = bot.download_file(file_info.file_path)
+                cursor.execute("INSERT INTO files (name, file) VALUES (?, ?)", (file_name + '.png', downloaded_file))
+                conn.commit()
+
+                bot.reply_to(message, f'Фото "{ file_name + ".png" }" успешно сохранено.')
+        except Exception as e:
+            bot.reply_to(message, f'Ошибка при сохранении файла: {e}')
+    else:
+        bot.reply_to(message, 'У вас нет прав для этого действия')
 
 
 bot.polling()
